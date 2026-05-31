99b98c578e603b9ed13ad8c10682cea669904ed3
Y NHEN ADRONG <listream843.maloc@gmail.com>
2026-05-31 10:47:24 +0700

    feat: add a new file

---
+++ database/shops/db.js
@@ -0,0 +1,75 @@
+// ==================== DỮ LIỆU SẢN PHẨM TẬP TRUNG ====================
+const productsData = [
+    {
+        id: "dpi-pro",
+        name: "DPI  IPHONE",
+        price: "50k",
+        fullPrice: "50.000 VNĐ",
+        originalPrice: "500k",
+        image: "https://i.ibb.co/3Yr5b4Hm/screenshot-2026-05-31-10-18-53-50-99c04817c0de5652397fc8b56c3b3817.jpg",
+        badge: "PRO",
+        badgeColor: "emerald",
+        features: [
+            "Fix rung tâm",
+            "Kéo tâm mượt",
+            "Tối ưu DPI",
+            "Headshot ổn định"
+        ],
+        devices: " iPhone",
+        deviceIcons: ["apple"]
+    },
+    {
+        id: "dpi-pro",
+        name: "DPI  ANDROID",
+        price: "50k",
+        fullPrice: "50.000 VNĐ",
+        originalPrice: "500k",
+        image: "https://i.ibb.co/3Yr5b4Hm/screenshot-2026-05-31-10-18-53-50-99c04817c0de5652397fc8b56c3b3817.jpg",
+        badge: "PRO",
+        badgeColor: "emerald",
+        features: [
+            "Fix rung tâm",
+            "Kéo tâm mượt",
+            "Tối ưu DPI",
+            "Headshot ổn định"
+        ],
+        devices: "Android ",
+        deviceIcons: ["android"]
+    },
+    {
+        id: "file-vip",
+        name: "AIMLOCK IPHONE",
+        price: "100k",
+        fullPrice: "100.000 VNĐ",
+        originalPrice: "1 Triệu",
+        image: "https://i.ibb.co/yFMV99B7/screenshot-2026-05-31-10-18-30-24-99c04817c0de5652397fc8b56c3b3817.jpg",
+        badge: "VIP",
+        badgeColor: "yellow",
+        features: [
+            "Fix lố đầu",
+            "full máu đỏ",
+            "Không rung tâm",
+            "không band acc"
+        ],
+        devices: "Iphone",
+        deviceIcons: ["apple"]
+    },
+    {
+        id: "file-vip",
+        name: "AIMLOCK ANDROID",
+        price: "100k",
+        fullPrice: "100.000 VNĐ",
+        originalPrice: "1 Triệu",
+        image: "https://i.ibb.co/yFMV99B7/screenshot-2026-05-31-10-18-30-24-99c04817c0de5652397fc8b56c3b3817.jpg",
+        badge: "VIP",
+        badgeColor: "yellow",
+        features: [
+            "Fix lố đầu",
+            "full máu đỏ",
+            "Không rung tâm",
+            "không band acc"
+        ],
+        devices: "Android",
+        deviceIcons: ["android"]
+    }
+];
